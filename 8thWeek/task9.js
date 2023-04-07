9f513cab4c156c90e183f02d8b174d9854ffc374
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-04-07 13:15:13 +0300

    Update task9.js

--- 8thWeek/task9.js
+++ 8thWeek/task9.js
@@ -2,7 +2,9 @@
 теперь проверяет по кол-ву ключей, после по наличию одинаковых ключей и равных значениях в них  */
 
 function IsEqual(first, second) {
-    if (Object.keys(first) == Object.keys(second))
+    if (Object.keys(first) !== Object.keys(second)){
+        return false;
+    }
     for (let key in first) {
         if (second.hasOwnProperty(key) && (first[key] !=== second[key])) {
             return false;
