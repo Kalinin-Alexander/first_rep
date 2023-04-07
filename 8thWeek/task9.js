a97ff2a38251ce315d00e05d037995219fdcea8b
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-04-07 09:36:24 +0300

    Update task9.js

--- 8thWeek/task9.js
+++ 8thWeek/task9.js
@@ -1,3 +1,11 @@
-function isEqual(first,second) {
-
-}
+function IsEqual(first, second) {
+    if (first.length !== second.length){
+        return false
+    }
+    for (let key in first) {
+        if (first.hasOwnProperty(key) && (first[key] !== second[key])) {
+            return false;
+        }          
+    }
+    return true
+}    
