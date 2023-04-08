290aa9b8af09f35f3e9afaadf4e70f76ef616ef4
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-04-08 14:21:05 +0300

    Update task11.js

--- 8thWeek/task11.js
+++ 8thWeek/task11.js
@@ -1,11 +1,15 @@
 /*  Внёс поправки
 теперь умножает на заданное число, возвращая новый массив  */
 
-function multiplyNumeric(obj, n) {
-    let newObj = _.cloneDeep(obj);
-    for (let key in newObj) {
-      if (typeof newObj[key] == 'number') {
-        return newObj[key] *= n;
-      }
+function multiplyNumeric(a, b) {
+    obj = {};
+    for (let k in a) {
+        obj[k] = a[k];
     }
-  }
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= b;
+        }
+    }
+    return obj
+}
