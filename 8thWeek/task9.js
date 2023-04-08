6edc6dbde2fd5a4876c49ea54bb4449f6e8edfe3
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-04-08 14:42:34 +0300

    Update task9.js

--- 8thWeek/task9.js
+++ 8thWeek/task9.js
@@ -1,14 +1,17 @@
 /*  Переделал
 теперь проверяет по кол-ву ключей, после по наличию одинаковых ключей и равных значениях в них  */
 
-function IsEqual(first, second) {
-    if (Object.keys(first) !== Object.keys(second)){
+function isEqual(first, second) {
+    if (Object.keys(first).length !== Object.keys(second).length) {
         return false;
     }
     for (let key in first) {
-        if (second.hasOwnProperty(key) && (first[key] !=== second[key])) {
+        if (second.hasOwnProperty(key) == false) {
+            return false;
+        }
+        if ((first[key] === second[key]) == false) {
             return false;
         }   
     }
     return true; 
-}   
+} 
