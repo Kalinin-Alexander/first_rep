d999745743ffe3e2804e5b9dd3cba49b738e15be
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-07-05 22:28:10 +0300

    Update time.js

--- 16thWeek/time.js
+++ 16thWeek/time.js
@@ -17,9 +17,9 @@ function getTimeRemaining(endtime) {
 
 //включение таймера
 function initializeClock(endtime) {
-    let clock = document.getElementById('timer');
-    let minutesSpan = clock.querySelector('.minutes');
-    let secondsSpan = clock.querySelector('.seconds');
+    //let clock = document.getElementsByClassName('timer-cnt_time');
+    let minutesSpan = document.querySelector('.minutes');
+    let secondsSpan = document.querySelector('.seconds');
    
     //обновление значения на табло
     function updateClock() {
@@ -35,31 +35,33 @@ function initializeClock(endtime) {
     timeinterval = setInterval(updateClock, 1000);
 }
 
+let started = false;
 //кнопка рестарта
+
 function restart() {
     clearInterval(timeinterval);
     diff = timeInMinutes * 60 * 1000;
     deadline = new Date(Date.parse(new Date()) + diff);
     initializeClock(deadline);
     started = true;
-    document.getElementById('starts').innerText = 'Stop';
+    document.getElementById('btn_txt').innerHTML = 'Stop';
     
 }
 
 //кнопка старт/стоп
-let started = false;
+
 function starts() {
     switch(started) {
         //первое нажатие должно запустить таймер
         case false: 
-            document.getElementById('starts').innerText = 'Stop';
+            document.getElementById('btn_txt').innerHTML = 'Stop';
             initializeClock(new Date(Date.parse(new Date()) + diff));
             started = true;
             break;
         //следующее нажатие после первого запоминает остаток времени и останавливает таймер
         case true: 
             clearInterval(timeinterval);
-            document.getElementById('starts').innerText = 'Start';
+            document.getElementById('btn_txt').innerHTML = 'Start';
             started = false;
             break;
     }
